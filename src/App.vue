6157b283c75d7d90a9ed9b2d53a84f0acd03302f
spinman81 <klimmalgin@yahoo.com>
2025-09-17 19:32:03 -0400

    Added video-js support

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
-<script setup lang="ts">
-import Todos from './components/Todos.vue'
+<script setup>
+import { ref } from "vue";
+import { NButton, NInput, NInputGroup, NDivider, NFlex } from "naive-ui";
+import VideoPlayer from "./components/VideoPlayer.vue";
+
+const url = ref("https://195181171156.cdn.etvnet.com/vod/AsnsW1TP0hmKuVms8dub3wKtM3Iyh90HsMcXNp6K7bplWcjZylGlmMicvGA6svLtkew3sxpcd_SHJfvtsc_VgQ/playlist.m3u8");
+const playerOptions = {
+  autoplay: false,
+  controls: true,
+  muted: true,
+  width: 960,
+  sources: [
+    {
+      src: url.value,
+      type: "application/x-mpegURL",
+    },
+  ],
+};
 </script>
 
 <template>
-  <main>
-    <Todos />
-  </main>
+    <video-player
+      :options="playerOptions"
+    />
 </template>
 
