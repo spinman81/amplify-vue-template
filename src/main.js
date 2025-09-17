ac636bee86b245e2da3a82ddeffd04ad8c6c3046
spinman81 <klimmalgin@yahoo.com>
2025-09-17 19:39:54 -0400

    Moved main.ts to main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// import { Amplify } from "aws-amplify";
-// import outputs from "../amplify_outputs.json";
-// Amplify.configure(outputs);
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+Amplify.configure(outputs);
 
 createApp(App).mount("#app");
